1719cdf14e3399b0b1fed567bbc4c87f573a9990
Vasilia Staykova <someonemyfriend@gmail.com>
2023-02-15 17:52:41 +0200

    Add files via upload

--- src/js/Application.js
+++ src/js/Application.js
@@ -11,15 +11,26 @@ export default class Application extends EventEmitter {
   constructor() {
     super();
 
+    this.beat = new Beat();
+    this._create();
+    this.emit(Application.events.READY);
+  }
+
+  _create() {
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
 
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = "Ah";
-
-    document.querySelector(".main").appendChild(message);
-
-    this.emit(Application.events.READY);
+      this._beat.addListener(Beat.events.BIT, (bit) => {
+        const message = document.createElement("div");
+        message.classList.add("message");
+        message.innerText = lyrics[count];
+        count++;
+        if(count==6) {
+          count=0;
+        }
+        document.querySelector(".main").appendChild(message);
+      })
+    // this.emit(Application.events.READY);
   }
+    
 }
